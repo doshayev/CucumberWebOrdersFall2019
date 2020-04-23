0c7e2e3df95616b416cc462fd4031a6db8d5eb57
Adil Doshayev <doshayev@gmail.com>
2020-04-22 20:43:45 -0500

    commit

--- src/test/java/com/weborders/step_definitions/Hooks.java
+++ src/test/java/com/weborders/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.weborders.step_definitions;
 
+import com.weborders.utilities.ConfigurationReader;
 import com.weborders.utilities.Driver;
 import io.cucumber.java.Scenario;
 import org.junit.After;
@@ -10,6 +11,9 @@ import org.openqa.selenium.TakesScreenshot;
 public class Hooks {
     @Before
     public void setup(Scenario scenario ){
+        System.out.println(":::: Starting test automation ::::");
+        System.out.println("Browser type :: "+ ConfigurationReader.getProperty("browser"));
+        System.out.println("Environment :: "+ ConfigurationReader.getProperty("url"));
         System.out.println("Test scenario :: "+ scenario.getName());
         Driver.getDriver().manage().window().maximize();
     }
